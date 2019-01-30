5f567c6f8d26802b733ed85a75b0c6bef69e2359
septembereatme <vitaliiseptember@gmail.com>
2019-01-30 20:45:17 +0200

    ver2

--- Учёба/Dictionary/user.py
+++ Учёба/Dictionary/user.py
@@ -6,3 +6,15 @@ user = {'name':'Vitaliy',
 for key,value in user.items():
     print('Key:', key)
     print('Value:', value)
+    print(' ' *100)
+
+
+favourite_languages = {'Alex':'C++',
+                       'Sara':'Java',
+                       'Lena':'C#',
+                       'Pavel Cat':'Ruby',
+                       'John Snow':'Python'
+                       }
+                    
+for name,language in favourite_languages.items():
+    print(name.title(),'s favourite languages:', language)
