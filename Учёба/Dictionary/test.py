a77dda8f080e56cfb0bd25cdf43ede8fba5a7da4
septembereatme <vitaliiseptember@gmail.com>
2019-01-30 20:34:46 +0200

    user

--- Учёба/Dictionary/test.py
+++
@@ -1,4 +0,0 @@
-import sys
-
-print('работа с Git')
-sys.exit(0)
---
+++ Учёба/Dictionary/user.py
@@ -0,0 +1,8 @@
+user = {'name':'Vitaliy',
+        'first':'Alex',
+        'second':'Lena'
+}
+
+for key,value in user.items():
+    print('Key:', key)
+    print('Value:', value)
