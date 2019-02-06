802c9b328686021fc8307a5b484e4c498c6334ff
septembereatme <vitaliiseptember@gmail.com>
2019-02-06 17:30:31 +0200

    06.02.2019

---
+++ Учёба/OOP/cartask.py
@@ -0,0 +1,29 @@
+class Car():
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year 
+        self.odometr = 0
+
+    def full_info(self):
+        full_name = ('\n Я езжу на ' + ' ' + self.make + ' ' + str(self.year) + ' года выпуска' 
+                        +' ' + self.model + ' модели.' )
+        return full_name.title()
+    
+    def show_odometr(self):
+        print('Пробег твоей каракатицы :', self.odometr)
+
+    def increment_odometr(self, km):
+        self.odometr += km
+
+    def update_odometr (self, miles):
+        if miles >= self.odometr:
+            self.odometr = miles
+        else:
+            print('Чувак не хитри, оматать не выйдет!')
+
+my_car = Car('Mitsubishi','Outlander','2010')
+print(my_car.full_info())
+my_car.increment_odometr(30)
+my_car.update_odometr(300)
+my_car.show_odometr()
