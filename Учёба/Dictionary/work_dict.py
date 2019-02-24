1282fffcb4af81d821f4b7a1f27f3cfb088af8d1
septembereatme <vitaliiseptember@gmail.com>
2019-02-24 20:10:44 +0200

    worK_dict

--- Учёба/Dictionary/work_dict.py
+++ Учёба/Dictionary/work_dict.py
@@ -2,6 +2,9 @@ person = {'name': 'Henry Ford',
           'Gender': 'Male',
           'position':'Data Scientist',
           'Planet':'Earth'}
+#Добавим ключ и значение в Словарь
+person['Friend'] = 'Kolya'
+person['Age'] = 33
 person_keys = []
 #сохраним ключи словаря person в person_keys
 for i in person.keys():
@@ -13,4 +16,7 @@ person_name = list(person['name'])
 #Юзаем обратный срез и убирем первые пять элементов
 print(person_name[:5:-1])
 for i in person_name:
+    print(i)
+
+for i in person.items():
     print(i)
