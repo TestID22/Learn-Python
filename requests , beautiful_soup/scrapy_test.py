d400dea317c313620b946559740598a187c63771
septembereatme <vitaliiseptember@gmail.com>
2019-02-25 17:07:33 +0200

    scrap

--- requests , beautiful_soup/scrapy_test.py
+++ requests , beautiful_soup/scrapy_test.py
@@ -2,12 +2,14 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 
-url = urlopen('http://news.meta.ua/')
+url = urlopen('http://novogrodovka-rada.gov.ua/')
 #создаём объект супа
 bso = BeautifulSoup(url)
 print(bso.a)
 #ищем по тегам выборки
-namelist = bso.findAll({'h1','h2','a'})
+word_tags = {'h1','h2','h3','a'}
+namelist = bso.findAll(word_tags)
+
 for name in namelist:
     #name.get_text() отделяет текс от тегов
     print(name.get_text())
