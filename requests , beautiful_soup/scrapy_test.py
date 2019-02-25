30577ffcc8e25fdc0f61390dfadacce5142bbfc1
septembereatme <vitaliiseptember@gmail.com>
2019-02-25 16:37:43 +0200

    scrapy_tag

--- requests , beautiful_soup/scrapy_test.py
+++ requests , beautiful_soup/scrapy_test.py
@@ -1,6 +1,14 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+
 url = urlopen('http://news.meta.ua/')
+#создаём объект супа
 bso = BeautifulSoup(url)
-print(bso)
+print(bso.a)
+#ищем по тегам выборки
+namelist = bso.findAll({'h1','h2','a'})
+for name in namelist:
+    #name.get_text() отделяет текс от тегов
+    print(name.get_text())
+input()
