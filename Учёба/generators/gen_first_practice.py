0f3090a2f6f799230bcd8a661ee3eed096210db8
septembereatme <vitaliiseptember@gmail.com>
2019-02-26 18:59:16 +0200

    generators_one_stroke

--- Учёба/generators/gen_first_practice.py
+++ Учёба/generators/gen_first_practice.py
@@ -6,11 +6,16 @@ print(l)
 #добавляем + 10 к списку l
 l = [x + 10 for x in l]
 print(l)
+
 #Читаем фалй 1.txt со всеми символами внутри
+#убираем в генераторе символ новых строк \n
+print('код из 3х строк')
 f = open('1.txt')
 lines = f.readlines()
-print(lines)
-
-#убираем в генераторе символ новых строк \n
 lines = [line.rstrip() for line in lines]
 print(lines)
+
+#Сокращаем код
+print('Короткий список = код в одной строке')
+short_code_lines = [line.rstrip() for line in open('1.txt')]
+print(short_code_lines)
