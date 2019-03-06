1ba0b7a49ec27f8010f464acfd25fd1e899b1d49
septembereatme <vitaliiseptember@gmail.com>
2019-03-06 18:20:31 +0200

    update_func_min

---
+++ My function/min_func.py
@@ -0,0 +1,8 @@
+def find_min_func(*args):
+    res = args[0]
+    for i in args[1:]:
+        if i < res:
+            res = i
+            return res
+
+print(find_min_func(1, 91, 20, -1, 23, 13))
--- Учёба/function/min_function.py
+++ Учёба/function/min_function.py
@@ -19,3 +19,17 @@ print(easy_min(2,5,98,2,10,-2))
 #поиск минимум через функцию min
 data_min = list(range(-100,100))
 print(min(data_min))
+
+############################
+#функция больше - меньше
+def max_num(x,y):
+    if x > y:
+        print(x,' Максимальное')
+    elif x < y:
+        print(y, ' Максимальное')
+    else:
+        print(x, y, 'РАВЕНСТВО')
+
+first_num = input('Введите первое число: ')
+second_num = input('Введите втророе число: ')
+max_num(int(first_num),int(second_num))
