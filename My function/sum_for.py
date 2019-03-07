afe8986a8a22db0f5eddfc61c2e783b746a46d67
septembereatme <vitaliiseptember@gmail.com>
2019-03-07 21:31:16 +0200

    update_my_func

--- My function/sum_for.py
+++ My function/sum_for.py
@@ -2,7 +2,7 @@ numbers = [1,4,5,7,80,23,634,4]
 sum_num = 0
 for i in numbers:
     sum_num += i
-print(sum_num)
+print('Сумма всех чисел списка numbers = ', sum_num)
 
 def min_func(*args):
     res = args[0]
@@ -10,9 +10,8 @@ def min_func(*args):
         if i < res:
             res = i
             return res
-            
-
-print(min_func(1,45,56,56,12,5,7,-2))
+        
+print('Минимальное число аргументов функции равно : ', min_func(1,45,56,56,12,5,7,-2))
 
 for_min = [1,9,24,6,1,3,-3]
 res = []
@@ -22,4 +21,4 @@ for i in for_min[1:]:
         start = i
         res.append(i)
         print(res)
-        
+###########################        
