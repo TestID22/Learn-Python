f2ff48013e0dda68a2ae866f3f4eefc69fa68286
septembereatme <vitaliiseptember@gmail.com>
2019-03-11 13:22:19 +0200

    classes_start

---
+++ Учёба/OOP/class_1.py
@@ -0,0 +1,14 @@
+class Employee():
+    def salary(self):
+        print('Зарплата - 30 тыс.руб')
+    def job(self):
+        print('QA engineer')
+class programmer(Employee):
+    def salary(self):
+        print('Вам не платят зарплату, а собственно за что вам платить')
+    def job(self):
+        print('Прогер')
+        
+
+
+        
---
+++ Учёба/OOP/importer_class_1.py
@@ -0,0 +1,8 @@
+import class_1
+        
+eliot = class_1.Employee()
+meg = class_1.programmer()
+
+workers = [eliot, meg]
+for i in workers:
+    print(i.salary())
