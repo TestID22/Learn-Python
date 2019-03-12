377b559d7259674425a574eb46874850e369dde3
septembereatme <vitaliiseptember@gmail.com>
2019-03-12 17:13:31 +0200

    OOP_thirdclass

--- Учёба/OOP/simpleclass.py
+++ Учёба/OOP/simpleclass.py
@@ -8,6 +8,16 @@ class SecondClass(FirstClass):
     def display(self):
         print('Текущее значение:"%s"' % self.data)
 
+class ThirdClass(SecondClass):
+    def __init__(self, value):
+        self.data = value
+    def __add__(self, other):
+        return ThirdClass(self.data + other)
+    def __str__(self):
+        return '[ThirdClass: %s]' % self.data
+    def mul(self, other):
+        self.data *= other
+
 
 x = FirstClass()
 y = FirstClass()
@@ -15,6 +25,17 @@ y = FirstClass()
 z = SecondClass()
 #
 z.setdata('42')
+
+#Создаём третий объект
+a = ThirdClass('Abc')
+a.display()
+print(a)
+#__str__ передаёт объекту возможность строчного типа данных
+b = a + 'xyz'
+print(b)
+#mul изменяет сам объект
+a.mul(3)
+print(a)
 
 x.setdata('Don Juan Matus')
 y.setdata(23)
