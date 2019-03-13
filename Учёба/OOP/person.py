81758238ddba8888c35643fa6199170d00d31359
septembereatme <vitaliiseptember@gmail.com>
2019-03-13 22:54:11 +0200

    person_class

--- Учёба/OOP/person.py
+++ Учёба/OOP/person.py
@@ -3,13 +3,20 @@ class Person():
         self.name = name
         self.job = job 
         self.pay = pay
+    def secondName(self):
+        return self.name.split()[-1]
+    def giveRaise(self, percent):
+        return int(self.pay * (1 + percent))
+
+
         
 if __name__ == "__main__":
-
     darling = Person('Darling')
-    elliot = Person('Elliot', job='Engineer',pay='40 000')
-
+    elliot = Person('Elliot Andersen', job='Engineer',pay=40000)
+#split разбивает строку в список по пробелам
+print('Фамилия сотрудника:', elliot.secondName())
+print('Поднимем зарплату на 10%', elliot.giveRaise(.10))
 print(darling.name, darling.pay)
 print(elliot.name, elliot.pay)
-
+print('Заработная плата сотрудника с учётом giveRaise', elliot.pay)
 
