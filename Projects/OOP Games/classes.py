8207935d7772f07b1edf98a334ac5ec430bff368
September <vitaliiseptember@gmail.com>
2019-03-14 18:33:43 +0200

    game_start_dev

--- Projects/OOP Games/classes.py
+++ Projects/OOP Games/classes.py
@@ -1,9 +1,9 @@
 import pygame
 
 class Player():
-    def __init__(self, screen):
-        self.image = pygame.image.load('D:\Code\Python\Projects\OOP Games\\player.png')
+    def __init__(self,):
+        self.image = pygame.image.load('D:\Code\Python\Projects\OOP Games\\my_asleep_reflection.png')
         self.name = 'Вова Кудрявый'
       
     def render(self, screen):
-        self.screen.blit(self.image(50,50),(300,40,50,50))
+        screen.blit(self.image,(50,340,100,50))
--- Projects/OOP Games/practice_oop_pygame.py
+++ Projects/OOP Games/practice_oop_pygame.py
@@ -4,17 +4,22 @@ from classes import Player
 
 pygame.init()
 screen = pygame.display.set_mode((800,500))
+pygame.display.set_caption('My Asleep Reflections')
 
 class Main():
     def __init__(self,screen):
         self.screen = screen
-        self.back = pygame.image.load('D:\Code\Python\Учёба\Practice\\back.jpg')
+        self.back = pygame.image.load('D:\Code\Python\Projects\OOP Games\\1.jpg')
+        self.player = Player()
         self.main_loop()
         
     def render(self):
         '''Отвечает за отрисовку элементов на экране'''
         self.screen.blit(self.back, (0, -300))
+        self.player.render(screen)
+
         pygame.display.flip()
+        pygame.mouse.set_visible(False)
 
     def main_loop(self):
         while True:
