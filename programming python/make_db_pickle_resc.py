75c440f6bfdfa098547d1da078fecb097fc9b0e2
September <vitaliiseptember@gmail.com>
2019-03-16 08:23:04 +0200

    prog_pickle_db

---
+++ programming python/make_db_pickle_resc.py
@@ -0,0 +1,8 @@
+from initdata import elliot, darling, tyrell
+import pickle
+
+for (key, record) in [('elliot', elliot),('darling',darling),('tyrell',tyrell)]:
+    recfile = open(key + '.pkl', 'wb')
+    pickle.dump = (record, recfile)
+    recfile.close()
+    
