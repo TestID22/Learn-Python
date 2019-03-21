d3a6c63827a166e80624bf90b4c9758ee5486673
September <vitaliiseptember@gmail.com>
2019-03-21 18:40:03 +0200

    threading

---
+++ Processes/threading.py
@@ -0,0 +1,18 @@
+import threading, time
+
+count = 0
+def adder():
+    global count
+    count = count + 1
+    time.sleep(0,5)
+    count += 1
+
+threads = []
+for i in range(100):
+    thread = threading.Thread(target=adder, args=())
+    thread.start()
+    threads.append(thread)
+
+for thread in threads:
+    thread.join()
+    print(count)
