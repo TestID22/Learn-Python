fc266c7bbf5893763689c1c2b2ce694344625905
September <vitaliiseptember@gmail.com>
2019-03-23 18:22:58 +0200

    big_pict

---
+++ Учёба/Practice/big_dit.py
@@ -0,0 +1,17 @@
+import sys, glob, os
+
+
+dirname = input('Введите имя дир: ') 
+
+allfile = []
+
+get_allfile = glob.glob(dirname + os.sep +'*.jpg')
+
+for picture in get_allfile:
+    get_size = os.path.getsize(picture)
+    allfile.append((get_size, picture))
+
+allfile.sort()
+
+print('Батюшки, а самая большая картинка это: ', allfile[-1:])
+print('Самая "лёгкая" картинка:', allfile[:1])
