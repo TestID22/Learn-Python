b90966830d4c3bff2ce46dcf67040279d03bd312
September <vitaliiseptember@gmail.com>
2019-03-26 17:34:55 +0200

    Creatre server_socket

---
+++ socket/sever_socket.py
@@ -0,0 +1,18 @@
+from socket import *
+
+myHost = 'localhost'
+myPort = 9000
+
+socket_object = socket(AF_INET, SOCK_STREAM)
+socket_object.bind((myHost, myPort))
+socket_object.listen(1)
+
+while True:
+    connecetion, adress = socket_object.accept()#ждём запрос
+    print(' server connect ', adress)
+
+    while True:
+        data = connecetion.recv(1024)
+        if not data: break
+        connecetion.send(b'Echo =>' + data)
+    connecetion.close()
