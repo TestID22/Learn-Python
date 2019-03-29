54d2fc1cbc013ca87584b4d0e49a4e6f0cc15cad
September <vitaliiseptember@gmail.com>
2019-03-29 20:36:45 +0200

    update_parse

--- requests , beautiful_soup/regex_mail.py
+++ requests , beautiful_soup/regex_mail.py
@@ -6,10 +6,7 @@ email_regex = re.compile('[A-Za-z0-9\._+]+@[A-Za-z]+\.(com)')
 file = [x.rstrip() for x in open('email_list.txt')]
 #конвертируем в тип Str
 e_finder = email_regex.search(str(file))
-
-email_adress = []
-
-for i in e_finder:
-    email_adress.append(i)
-
-print(email_adress)
+####################################### Работа над сохранением полученных словарей
+parse_email = open('emails.txt', 'w')
+parse_email.write(e_finder)
+print(e_finder)
