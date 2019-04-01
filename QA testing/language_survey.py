2dd7d12eeef25465ba376a03ed368dbf13193eb6
September <vitaliiseptember@gmail.com>
2019-04-01 16:48:02 +0300

    test_class

---
+++ QA testing/language_survey.py
@@ -0,0 +1,2 @@
+from survey import AnonymousSurvey
+
---
+++ QA testing/survey.py
@@ -0,0 +1,20 @@
+class AnonymousSurvey():
+    '''Сбор анонимных ответов на вопросы'''
+    def __init__(self, question):
+        self.question = question
+        self.responses = []
+
+    def show_question(self):
+        '''Выводит вопрос'''
+        print(question)
+
+    def store_response(self, new_response):
+        #СОХРАНЯЕТ ОТВЕТ В СПИСКЕ
+        self.responses.append(new_response)
+
+    def show_result(self):
+        #Выводит все результаты
+        print('Результат опроса')
+        for response in self.responses:
+            print(' +',response)       
+
