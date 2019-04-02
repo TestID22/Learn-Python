b56d9ad838d3f25959a408a9db60ff202eda273b
September <vitaliiseptember@gmail.com>
2019-04-02 18:53:47 +0300

    class_setUp_method

--- QA testing/test_survey.py
+++ QA testing/test_survey.py
@@ -1,28 +1,36 @@
 import unittest
 from survey import AnonymousSurvey
 
+
 class TestAnonymousSurvey(unittest.TestCase):
+    def setUp(self):
+        '''
+        Создание вопроса, самого опроса(объекта класса),
+        и вариантов ответов
+        '''
+        question = 'What first Language do you speak?'
+
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['English','Spanish', 'Python']
+
+
     '''Тетируем класс'''
     def test_store_single_response(self):
         question = 'What first Language do you speak?'
         #create obj a class AnonymousSurvey
         my_survey = AnonymousSurvey(question)
-        my_survey.store_response('English')
-
-        self.assertIn('English', my_survey.responses)
+        self.my_survey.store_response(self.responses[0])
+        self.assertIn(self.responses[0], self.responses)
 
     def test_store_three_response(self):
         '''тестируем на наличие трёх элементов в списке'''
-        question = 'What first Language do you speak?'
-        my_survey = AnonymousSurvey(question)
-        responses = ['English', 'Spanish', 'Python']
         #Из списка responses берём Языки и добавляем в список, 
         # при помощи метода my_survey.store_response 
-        for response in responses:
-            my_survey.store_response(response)
+        for response in self.responses:
+            self.my_survey.store_response(response)
         #Проверяем есть ли в списке каждый элемент из списка responses - ГАДДЭМ!!!
-        for response in responses:
-            self.assertIn(response, my_survey.responses) 
+        for response in self.responses:
+            self.assertIn(response,self.my_survey.responses) 
                
 
 unittest.main()
