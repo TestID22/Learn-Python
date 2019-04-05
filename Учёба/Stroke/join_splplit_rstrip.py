6ced994eaabbe493917b8a36c9a7e10f04499159
September <vitaliiseptember@gmail.com>
2019-04-05 13:17:21 +0300

    join_split_Stroke_method

---
+++ Учёба/Stroke/join_splplit_rstrip.py
@@ -0,0 +1,6 @@
+
+november = 'remember remember the first of november'.split()
+print(november)
+#Using hard skills and method join
+september = '-'.join('There can be only one'.split())
+print(september)
