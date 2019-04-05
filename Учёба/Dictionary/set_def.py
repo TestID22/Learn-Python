8deefc20c63c063299f278490fcf29a17969c154
September <vitaliiseptember@gmail.com>
2019-04-05 12:08:48 +0300

    dictionary_add__

---
+++ Учёба/Dictionary/set_def.py
@@ -0,0 +1,18 @@
+
+very_dif_dictionary = {'elliot':'hacker', 'age': 23, 'pay': 40000 }
+
+very_dif_dictionary.setdefault('status', 'alive')
+
+for key, value in very_dif_dictionary.items():
+    print('Ключь:', key, '\n')
+    print('Значениt:', value, '\n')
+
+print(
+''' 
+ssssss  eeeee  ppppp  tttttttt
+ss      ee     p   p     tt
+  ss    eee    ppppp     tt
+    ss  ee     p         tt
+ssssss  eeeee  p         tt
+
+''')
