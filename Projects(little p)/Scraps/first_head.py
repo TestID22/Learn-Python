7568cd8e1c813dafca5a8df9f6725619c0d7a72f
September <vitaliiseptember@gmail.com>
2019-04-05 12:42:41 +0300

    update_scrapy_letters_finder_sort

--- Projects(little p)/Scraps/first_head.py
+++ Projects(little p)/Scraps/first_head.py
@@ -1,12 +1,12 @@
 #Используется для нахождения повторябщихся символов
 #поиск букв в слове или предложении
-vowels = ['a','b','h','g','s','o',]
-word = 'Jon Snow'
+vowels = ['a','b','h','g','s','o', '1']
+word = 'Jon Snow and Khalisy 1'
 found = []
 
 for letter in word.lower():
     if letter in vowels:
         if letter not in found:
             found.append(letter)
-
+found.sort()
 print(found)
