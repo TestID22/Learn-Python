1caa60f17fc4aaac279f0fd6d94a97013819728e
September <vitaliiseptember@gmail.com>
2019-04-06 20:25:08 +0300

    mers_rio

--- Projects(little p)/Scraps/scrap_mers_rio.py
+++ Projects(little p)/Scraps/scrap_mers_rio.py
@@ -5,5 +5,5 @@ site = urlopen('https://auto.ria.com/car/mercedes-benz/')
 soup = BeautifulSoup(site.read())
 get_my_mers = soup.findAll('span',{'class':'blue bold'})
 for mers in get_my_mers:
-    print(mers)
+    print(mers.get_text())#get_text() отделяет текст от тегов HTML
 
