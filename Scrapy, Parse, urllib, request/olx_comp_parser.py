f6fdf832dcb6b4756914230a8b395e462b7caabe
September <vitaliiseptember@gmail.com>
2019-04-12 16:38:18 +0300

    get_all_links

--- Scrapy, Parse, urllib, request/olx_comp_parser.py
+++ Scrapy, Parse, urllib, request/olx_comp_parser.py
@@ -8,13 +8,34 @@ headers = {
             'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; en-en) \
             AppleWebKit/533.19.4 (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4'
             }
+#Захват страницы, возьмём код HTML
 def getHtml(url):
     html = requests.get(url, headers=headers) #response - это объект реквеста запроса
     return html.text # метод текст возвращает html
 
+#Захват ссылок на товар
+def getAllLinks(html):
+    soup = BeautifulSoup(html)
+    find_links = soup.find('table', id='offers_table').find_all('tr', class_='wrap')
+    links = []
+    for tr in find_links:
+        a = tr.find('a').get('href')
+        links.append(a)
 
-def main():
-    print(getHtml(url))
+    return links
+
     
+
+            
+def main():
+    html = getHtml(url)
+    parse_links = getAllLinks(html)
+    for i in parse_links:
+        print(i)
+    
+
+
+
+
 if __name__ == "__main__":
     main()
