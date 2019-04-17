e4ac3e6a809a2c791af1bcb9a4086b5e3c32620b
September <vitaliiseptember@gmail.com>
2019-04-17 16:06:12 +0300

    smtp_mime

---
+++ smtplib (email)/smtp_mime.py
@@ -0,0 +1,15 @@
+import smtplib
+from email.mime.text import MIMEText
+
+msg = MIMEText('This is a Text for you')
+
+msg['Subject'] = 'Тут Тема Письма'
+msg['From'] = 'vitaliiseptember@gmail.com'
+msg['To'] = 'vitaliiseptember@gmail.com'
+
+s = smtplib.SMTP('smtp.gmail.com', 587)
+s.ehlo()
+s.starttls()
+s.login('vitaliiseptember@gmail.com','mypass')
+s.send_message(msg)
+s.quit
