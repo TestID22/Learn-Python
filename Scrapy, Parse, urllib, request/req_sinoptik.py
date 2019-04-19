85776fad9ccf3eed44fa5806fea01caff62eb7ca
September <vitaliiseptember@gmail.com>
2019-04-19 15:43:37 +0300

    sinoptic_make_func

--- Scrapy, Parse, urllib, request/req_sinoptik.py
+++ Scrapy, Parse, urllib, request/req_sinoptik.py
@@ -1,11 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
+#Погода в Новогродовке Программа для частного пользования, если тебе лень 
+#посмотреть в окно
+def get_html(url):
+    html = requests.get(url)
+    return html.text
 
-html = requests.get('https://sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%BD%D0%BE%D0%B2%D0%BE%D0%B3%D1%80%D0%BE%D0%B4%D0%BE%D0%B2%D0%BA%D0%B0')
-soup = BeautifulSoup(html.text)
+def get_data(html):
+    soup = BeautifulSoup(html)
+    get_city = soup.find('div', class_='cityName').find('h1').text
+    get_temp = soup.find('p', class_='today-temp').text
+    get_text = soup.find('div', class_='description').text
+    
+    return get_city, get_temp, get_text
 
-get_city = soup.find('div', class_='cityName').find('h1').text
-get_temp = soup.find('tr', class_='temperature').find('td', class_='p5').text
-get_text = soup.find('div', class_='description').text
-print(get_city)
-print(get_text, ' На данный момент температура воздуха составит:' ,get_temp)
+def main():
+    url = 'https://sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%BD%D0%BE%D0%B2%D0%BE%D0%B3%D1%80%D0%BE%D0%B4%D0%BE%D0%B2%D0%BA%D0%B0'
+    html = get_html(url) # work
+    data = get_data(html)
+    print('\n' + data[0] + '\n')
+    print(data[2])
+    print('Температура воздуха:', data[1])
+    input('Нажми "Enter" для выхода.')
+
+if __name__ == "__main__":
+    main()
