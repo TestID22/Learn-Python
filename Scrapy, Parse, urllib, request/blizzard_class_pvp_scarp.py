1bb95f8ebd4c64512e13b73478b16e4987460c59
September <vitaliiseptember@gmail.com>
2019-04-19 12:07:38 +0300

    blizz_top_class_scraPY

---
+++ Scrapy, Parse, urllib, request/blizzard_class_pvp_scarp.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_html(url):
+    html = requests.get(url)
+    return html.text #возвращает страшно некрасивый HTML 
+
+def get_name_links(html):
+    soup = BeautifulSoup(html)
+    find_table = soup.findAll('div', class_="SortTable-col SortTable-data")
+    links = []
+    for link in find_table:
+        a = link.find('a', class_='Link').get('href')
+        full_url = 'https://worldofwarcraft.com' + a
+        links.append(full_url)
+    return links
+   
+def get_data(html):
+    soup = BeautifulSoup(html)
+    find_class = soup.find('div', class_='CharacterHeader-detail').text
+
+    return find_class
+
+def main():
+    url = 'https://worldofwarcraft.com/en-us/game/pvp/leaderboards/2v2' 
+    html = get_html(url)
+    links = get_name_links(html) # stage 2 end 
+    for link in links:
+        html_name = get_html(link)
+        data = get_data(html_name)
+        print(data)
+
+if __name__ == "__main__":
+    main()
--- smtplib (email)/smtp_mime.py
+++ smtplib (email)/smtp_mime.py
@@ -8,8 +8,8 @@ msg['From'] = 'vitaliiseptember@gmail.com'
 msg['To'] = 'vitaliiseptember@gmail.com'
 
 s = smtplib.SMTP('smtp.gmail.com', 587)
-s.ehlo()
-s.starttls()
+s.ehlo() #приветсвие
+s.starttls() #шифрование
 s.login('vitaliiseptember@gmail.com','mypass')
 s.send_message(msg)
 s.quit
