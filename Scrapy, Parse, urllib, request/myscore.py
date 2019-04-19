9c3b31c2aea729c070def6fec570c4ae9bf43af2
September <vitaliiseptember@gmail.com>
2019-04-19 18:39:09 +0300

    not_work

--- Scrapy, Parse, urllib, request/myscore.py
+++ Scrapy, Parse, urllib, request/myscore.py
@@ -8,14 +8,17 @@ def get_html(url):
 
 def get_game(html):
     soup = BeautifulSoup(html, 'html.parser')
-    get_team = soup.findAll('td', class_='cell_ab team-home  bold')
-    return get_team
+    get_team = soup.find('table', class_='soccer').find_all('span')
+    td = []
+    for i in get_team:
+        td.append(i)
+    return td
 
 def main():
     url = 'https://www.myscore.com.ua/football/italy/serie-a/'
     html = get_html(url)
     data = get_game(html)
-
+    print(type(data))
     for i in data:
         print(i)
     
