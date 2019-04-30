df358b8e9fea7265a716525568422de41b055f60
September <vitaliiseptember@gmail.com>
2019-04-30 12:38:41 +0300

    'tkinter_game'

---
+++ tkinter(GUI)/game_tkinter.py
@@ -0,0 +1,36 @@
+from tkinter import *
+import time
+import random
+
+class Ball():
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_oval(10,10,25,25, fill = color)
+        self.canvas.move(self.id, 245, 100)
+        self.x = 0
+        self.y = -1
+        self.canvas_height = self.canvas.winfo_height()
+
+    def draw(self):
+        self.canvas.move(self.id, self.x, self.y)
+        pos = self.canvas.coords(self.id)
+        print(pos[1])
+        if pos[1] <= 0:
+            self.y = 1
+        if pos[3] >= 400:
+            self.y = -1
+
+
+tk = Tk()
+tk.title('GameOfThrones')#Заголовок окна
+tk.resizable(0,0) #Неизменяемый размер окна
+tk.wm_attributes("-topmost", 1)
+canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness = 0)
+canvas.pack()
+
+ball = Ball(canvas, 'green')
+while True:
+    tk.update()
+    ball.draw()
+    time.sleep(0.01)
+    
