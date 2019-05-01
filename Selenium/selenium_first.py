d5f4f9fbfc5e5dbb71c1edd4eaac15288dc1fce7
September <vitaliiseptember@gmail.com>
2019-05-01 19:10:07 +0300

    selen_chrome

--- Selenium/selenium_first.py
+++ Selenium/selenium_first.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
-d = webdriver.Chrome(r'D:\chromedriver_win32\chromedriver.exe')
+d = webdriver.Chrome(r'D:\chromedriver_win32\chromedriver')
 d.get('https://duckduckgo.com/')
 d.find_element_by_id('search_form_input_homepage')
-d.
+time.sleep(5)
+d.send_keys_to_element('sss')
