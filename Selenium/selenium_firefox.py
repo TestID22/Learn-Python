302f988f88764053e1c95904c50c7fd31b58eb48
September <vitaliiseptember@gmail.com>
2019-05-02 14:16:19 +0300

    selen_update_driver

--- Selenium/selenium_firefox.py
+++ Selenium/selenium_firefox.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 import time 
 
-webdriver = webdriver.PhantomJS(r'D:\phantomjs-2.1.1-windows\phantomjs-2.1.1-windows\bin\phantomjs.exe')
+webdriver = webdriver.PhantomJS(r'D:\phantomjs-2.1.1-windows\phantomjs-2.1.1-windows\bin\phantomjs')
 webdriver.get('https://habr.com/ru/post/250921/')
 print(webdriver.find_element_by_tag_name('h1').text)
--- tkinter(GUI)/test_picture_tkinter.py
+++ tkinter(GUI)/test_picture_tkinter.py
@@ -9,20 +9,3 @@ tk.resizable(0,0)
 canvas = Canvas(tk, width=1000, height=900, bd=0)
 canvas.pack()
 image = PhotoImage(file=r'‪D:\Арт\player1.gif')
-
-class Ball():
-    def __init__(self, canvas):
-        self.canvas = canvase
-        self.id = canvas.create_image(0, 0, anchor = NW, image = image)
-
-    def draw(self):
-        pass
-
-
-
-
-
-ball = Ball(canvas)
-while 1:
-    tk.update()
-    ball.draw()
