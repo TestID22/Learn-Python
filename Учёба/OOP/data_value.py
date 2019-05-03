691284ec0aa9fa80d5022accaba7fd20a09ee324
September <vitaliiseptember@gmail.com>
2019-05-03 17:44:59 +0300

    'OOP'

--- Учёба/OOP/data_value.py
+++ Учёба/OOP/data_value.py
@@ -10,12 +10,21 @@ class SecondClass(FirstClass):
     def display(self):
         print('Curent value %s еее' % self.data) #вставка в страку данных по средствам %s
 
+class ThirdClass(SecondClass):
+    def __init__(self, data):
+        self.data = data
+    def __add__(self, other):
+        return(ThirdClass.data + other)
+    def __str__(self):
+        return 'ThirdClass %s' % self.data
 
 
 x = FirstClass('Garry')
 y = SecondClass('Hammy')
 y.set_data('Хелоу')
 y.display()
+z = ThirdClass('Куль')
+z.display()
 #Передаём значение вне класса
 x.data = 'Ку'
 x.display()
