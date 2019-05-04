a60a3e8e50cedd68cd7280bf505b0bb412367b9a
September <vitaliiseptember@gmail.com>
2019-05-04 13:05:38 +0300

    find_word

---
+++ My function and Algorithm/find_word.py
@@ -0,0 +1,22 @@
+import os
+
+
+f = open('testtext.txt', 'w')
+f.write('hack hack buerak email')
+f.close
+
+word_list = ['h', 'hack']
+
+f = open('testtext.txt', 'r')
+read = f.readline()
+formated_read = read.split()
+
+
+words_num = 0
+for word in word_list:
+    for line in formated_read:
+        if word == line:
+            words_num += 1
+
+
+print('Количество потворяющихся слов' , words_num)
