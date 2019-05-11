3b5b2b0efb4b27dd2f333eacf14a68d8f94fb91e
September <vitaliiseptember@gmail.com>
2019-05-11 17:11:46 +0300

    curency_from_c++

---
+++ My function and Algorithm/conv_currency.py
@@ -0,0 +1,20 @@
+
+print('''
+u   u aaaa h   h  tttttttt ooooooo    $$  $$  $$$$$  $$$$
+u   u a  a h   h     tt    oo   oo    $$  $$  $$     $$  $$
+u   u aaaa hhhhh     tt    oo   oo    $$  $$     $$  $$  $$
+uuuuu a  a h   h     tt    ooooooo    $$$$$$  $$$$$  $$$$
+
+''')
+
+
+print("Конвертер Гривны в Долар и Обратно")
+uah_per_usd = 0.038
+cost = float(input("Введите вашу сумму"))
+currency = input("Введите для гривны 'g' для доляра 'd' ")
+
+if currency == 'g':
+    print("За твои", cost , "гривен дадут", cost * uah_per_usd, "бачей")
+elif currency == 'd':
+     print("За твои", cost , "баксов дадут", cost * uah_per_usd, "гривастых")
+    
