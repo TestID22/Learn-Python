60ab64ec5278586af4938ebae483470921fa8fd2
September <vitaliiseptember@gmail.com>
2019-05-11 14:04:42 +0300

    free_msg_mts_ukr

--- pyautogui/mts_sms.py
+++ pyautogui/mts_sms.py
@@ -1,7 +1,13 @@
 import pyautogui, os, time
 
+
 os.startfile('https://www.vodafone.ua/ru#sms-form')
 time.sleep(10)
 pyautogui.moveTo(555, 666)
 time.sleep(2)
 pyautogui.click()
+time.sleep(2)
+pyautogui.click()
+pyautogui.moveTo(555,500).click()
+time.sleep(10)
+pyautogui.typewrite("Testing test") 
