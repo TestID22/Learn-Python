a0374824d2c231e9961584f1bf1f900fdf17062b
September <vitaliiseptember@gmail.com>
2019-05-11 17:14:28 +0300

    update_currency_logic_error_fix

--- My function and Algorithm/conv_currency.py
+++ My function and Algorithm/conv_currency.py
@@ -10,11 +10,12 @@ uuuuu a  a h   h     tt    ooooooo    $$$$$$  $$$$$  $$$$
 
 print("Конвертер Гривны в Долар и Обратно")
 uah_per_usd = 0.038
-cost = float(input("Введите вашу сумму"))
+cost = float(input("Введите вашу сумму "))
 currency = input("Введите для гривны 'g' для доляра 'd' ")
 
 if currency == 'g':
     print("За твои", cost , "гривен дадут", cost * uah_per_usd, "бачей")
 elif currency == 'd':
-     print("За твои", cost , "баксов дадут", cost * uah_per_usd, "гривастых")
-    
+    print("За твои", cost , "баксов дадут", cost / uah_per_usd, "гривастых")
+else: 
+    print("Четстно говоря, я такого ещё не видал")
